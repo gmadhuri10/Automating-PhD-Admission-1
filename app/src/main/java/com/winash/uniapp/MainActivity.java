c587e79688680b654947ed6448c6dfc7dfca69d8
ashwinraj01 <ashwinraj2304@gmail.com>
2022-03-15 14:32:34 +0530

    simple changes

--- app/src/main/java/com/winash/uniapp/MainActivity.java
+++ app/src/main/java/com/winash/uniapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.winash.uniapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register_applicant);
+    }
+    public void check(View view){
+        setContentView(R.layout.login);
     }
 }
