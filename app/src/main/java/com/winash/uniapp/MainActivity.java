ef9d49a4a4929490413a351afda6c27317e3c403
ashwinraj01 <ashwinraj2304@gmail.com>
2022-03-15 22:33:29 +0530

    started with backend

--- app/src/main/java/com/winash/uniapp/MainActivity.java
+++ app/src/main/java/com/winash/uniapp/MainActivity.java
@@ -2,17 +2,31 @@ package com.winash.uniapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView Register,Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_applicant);
+        setContentView(R.layout.activity_main);
+        Register=(TextView) findViewById(R.id.Register);
+        Login=(TextView) findViewById(R.id.Login);
+        Register.setOnClickListener(this);
+
     }
-    public void check(View view){
-        setContentView(R.layout.login);
-    }
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.Register:
+                    startActivity(new Intent(this,RegisterUser.class));
+                    break;
+                case R.id.Login:
+                    startActivity(new Intent(this,LoginUser.class));
+                    break;
+            }
+        }
 }
